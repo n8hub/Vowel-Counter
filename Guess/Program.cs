f30b5263efcd1b6b675ba17281b10cab3d3c4f1d
n8hub <Roberts-n4@ulster.ac.uk>
2025-10-22 12:44:54 +0100

    Add files via upload

---
+++ Guess/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+class Vowels
+{
+    public static void Main()
+    {
+        List<char> vowels = new List<char> { 'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U', };
+        Console.WriteLine("Enter a string");
+        string word = Console.ReadLine();
+        int count = 0;
+        foreach (char v in word)
+        {
+            if(vowels.Contains(v))
+            {  count++; }
+
+            
+            
+        }
+        Console.WriteLine($"There are {count} vowels in your word");
+    }
+}
